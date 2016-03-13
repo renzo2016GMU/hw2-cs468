75844ab0b0e14460fe48725b9b56c0f931ab2a5d
Renzo T <rt@rs-MacBook-Pro.fios-router.home>
2016-03-13 14:36:18 -0400

    Reads key instead of seed

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ hw2:
 	gcc -I/usr/local/opt/openssl/include -o  hw2 -lcrypto hw2.c
 
 pipeline1: 
-	./hw2 CS468-HW2.txt out.txt
+	./hw2 deskey.txt CS468-HW2.txt out.txt
 clean:
 	-rm hw2 
--- hw2.c
+++ hw2.c
@@ -80,6 +80,70 @@ read_and_show_the_file (char const* path, char **buf, bool add_nul)
 }
 
 long
+read_and_show_the_file_key (char const* path, char **buf, bool add_nul)
+{
+  FILE *fp;
+  size_t fsz;
+  long off_end;
+  int rc;
+
+  /* Open the file */
+  fp = fopen (path, "r");
+  if ( NULL == fp)
+    {
+      return -1L;
+    }
+
+  /* Seek to the end of the file */
+  rc = fseek (fp, 0L, SEEK_END);
+  if (0 != rc)
+    {
+      return -1L;
+    }
+
+  /* Byte offset to the end of the file (size) */
+  if (0 > (off_end = ftell (fp)))
+    {
+      return -1L;
+    }
+  fsz = (size_t) off_end;
+
+  /* Allocate a buffer to hold the whole file */
+  *buf = malloc (fsz + (int) add_nul);
+  if ( NULL == *buf)
+    {
+      return -1L;
+    }
+
+  /* Rewind file pointer to start of file */
+  rewind (fp);
+
+  /* Slurp file into buffer */
+  if (fsz != fread (*buf, 1, fsz, fp))
+    {
+      free (*buf);
+      return -1L;
+    }
+
+  /* Close the file */
+  if ( EOF == fclose (fp))
+    {
+      free (*buf);
+      return -1L;
+    }
+
+  if (add_nul)
+    {
+      /* Make sure the buffer is NUL-terminated, just in case */
+      *buf[fsz] = '\0';
+    }
+
+  /* Return the file size */
+  return (long) fsz;
+}
+
+
+long
 write_and_show_the_file (char const* path, char **buf, bool add_nul)
 {
   FILE *fp;
@@ -133,33 +197,33 @@ write_and_show_the_file (char const* path, char **buf, bool add_nul)
 int
 main (int argc, char *argv[])
 {
+  char *in_file_key   = argv[1];
+  char *in_file_name  = argv[2];
+  char *out_file_name = argv[3];
 
-  char *in_file_name  = argv[1];
-  char *out_file_name = argv[2];
-  char *in_file_key   = argv[3];
-  char *inputarray; 			//inputarray will hold all chars of in_file_name
-  char out[BUFSIZE], back[BUFSIZE];
+  char *inputarray, *keyarray; 		// will hold all chars of in_file_name
+  unsigned char out[BUFSIZE], back[BUFSIZE];
   long file_size;
+  long key_size;
 
-  DES_cblock key;
-  DES_cblock seed =
-    { 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10 };
   DES_key_schedule keysched;
-
-  RAND_seed (seed, sizeof(DES_cblock));
+  DES_cblock key;
   DES_random_key (&key);
   DES_set_key ((C_Block *) key, &keysched);
-
 
   memset(back, 0, sizeof(back));
   memset(out, 0, sizeof(out));
   unsigned char *e = out;
+  unsigned char *f = key;
 
 /*************************************************************************************
  *************************************************************************************
  * Step 1: read_and_show_the_file function reads *in_file_name, and stores it in in_file_name
  *************************************************************************************
  **************************************************************************************/
+
+  printf("\n------------------------------------------------------------------------------------------------\n----------------------------------------Start of Program----------------------------------------\n------------------------------------------------------------------------------------------------\n");
+  printf ("\n1. Plain_text_input: ");
   file_size = read_and_show_the_file (in_file_name, &inputarray, false);
 
   if (file_size < 0L)
@@ -168,13 +232,36 @@ main (int argc, char *argv[])
       return 1;
     }
   (void) fwrite (inputarray, 1, file_size, stdout);
-  printf ("\n1. Plain_text: %s ", inputarray);
+
+
+  printf ("\n1. Plain_text_Key:  ");
+  key_size = read_and_show_the_file_key (in_file_key, &keyarray, false);
+
+  if (key_size < 0L)
+    {
+      perror ("File read failed");
+      return 1;
+    }
+  (void) fwrite (keyarray, 1, key_size, stdout);
+
+  DES_cblock plaintext_value_key;
+  DES_string_to_key(inputarray, &plaintext_value_key);  //Convert key from char to hex key format
+//  while (*f)
+//    {
+//      fprintf (fp, " %02x", *e++);
+//      printf (" %02x", *e++);
+//    };
+//  printf("\n------------------------\n");
+
+
+
 
   /*************************************************************************************
    *************************************************************************************
    * Step 2: Create and write in out_file_name the hexadecimal value after inputfile has being encrypted
    *************************************************************************************
    **************************************************************************************/
+//encryption occurs  1 out gets cyphertext
 
   FILE *fp;
   fp = fopen (out_file_name, "wb");
